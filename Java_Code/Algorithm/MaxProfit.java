8c82e782bac4add370354795c0bc52f8d682c9e3
King Of Code <sb921204@naver.com>
2020-08-01 22:18:43 +0900

    java code

--- Java_Code/Algorithm/MaxProfit.java
+++ Java_Code/Algorithm/MaxProfit.java
@@ -1,4 +1,16 @@
 
-public class MaxProfit {
-
+class MaxProfit {
+	public int solution(int[] A) {
+		int min = (A.length > 0) ? A[0] : 0;
+		int result = 0;
+		for(int num : A) {
+			if(num <= min) {
+				min = num;
+			}
+			if(result < num - min) {
+				result = num - min;
+			}
+		}
+		return result;
+	}
 }
